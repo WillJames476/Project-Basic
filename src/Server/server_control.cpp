6131061a8d2f0b20d171c7f290009858568d2669
WillJames476 <williamjames4@tutanota.com>
2022-02-09 01:00:38 +0800

    builded the account functionality at the minimum goal

--- src/Server/server_control.cpp
+++ src/Server/server_control.cpp
@@ -10,17 +10,25 @@
 #include "file_agregates.h"
 
 
-std::string get_control(const std::string& message
+std::string get_control(std::istringstream& message
                 , const Control_agregate& controls);
 
-std::string put_control(const std::string& message
+std::string put_control(std::istringstream& message
                     , const Control_agregate& controls);
 
-std::string delete_control(const std::string& message
+std::string delete_control(std::istringstream& message
                     , const Control_agregate& controls);
 
 void file_control(const std::string& mode
                 , File_agregate& file);
+
+std::string apply_function(const std::vector<std::string>& list
+                        , size_t expected_size
+                        , const Control_agregate& controls
+                        , std::string(*applier)
+                            (const Control_agregate& ,const std::vector<std::string>&));
+
+std::vector<std::string> extract_args(std::istringstream& message);
 
 std::string server_control(const std::string& message)
 {
@@ -31,22 +39,20 @@ std::string server_control(const std::string& message)
     file_control("READ", files);
 
     std::istringstream method_extract(message);
-    std::string method{}, to_return{message}, post_extract{};
-
+    std::string method{}, to_return{message};
     method_extract >> method;
-    post_extract = method_extract.str();
 
     if(method == "GET")
     {
-        to_return = get_control(post_extract, controls);
+        to_return = get_control(method_extract, controls);
     }
     else if(method == "PUT")
     {
-        to_return = put_control(post_extract, controls);
+        to_return = put_control(method_extract, controls);
     }
     else if(method == "DELETE")
     {
-        to_return = delete_control(post_extract, controls);
+        to_return = delete_control(method_extract, controls);
     }
 
     file_control("WRITE", files);
@@ -54,49 +60,52 @@ std::string server_control(const std::string& message)
     return to_return;
 }
 
-std::string get_control(const std::string& message
+std::string get_control(std::istringstream& message
                 , const Control_agregate& controls)
 {
     std::string application{};
-    std::istringstream arguments(message);
-
-    arguments >> application >> application;
+    message >> application;
+    auto args{extract_args(message)};
 
     if(application == "--account")
     {
-        application = controls.account.get_from_list({"hello", "hi"});
+        application = apply_function(args , 2
+                        , controls , [](const auto& x, const auto& y)
+                            {return x.account.get_from_list({y[0], y[1]});});
     }
 
     return application;
 }
 
-std::string put_control(const std::string& message
+std::string put_control(std::istringstream& message
                         , const Control_agregate& controls)
 {
     std::string application{};
-    std::istringstream arguments(message);
-
-    arguments >> application >> application;
+    message >> application;
+    auto args{extract_args(message)};
 
     if(application == "--account")
     {
-        application = controls.account.add_to_list({"hello", "hi"});
+        application = apply_function(args , 2
+                        , controls , [](const auto& x, const auto& y)
+                            {return x.account.add_to_list({y[0], y[1]});});
     }
 
     return application;
 }
 
-std::string delete_control(const std::string& message
+std::string delete_control(std::istringstream& message
                         , const Control_agregate& controls)
 {
     std::string application{};
-    std::istringstream arguments(message);
-
-    arguments >> application >> application;
+    message >> application;
+    auto args{extract_args(message)};
 
     if(application == "--account")
     {
-        application = controls.account.remove_from_list({"hello","hi"});
+        application = apply_function(args , 2
+                        , controls , [](const auto& x, const auto& y)
+                            {return x.account.remove_from_list({y[0], y[1]});});
     }
 
     return application;
@@ -113,4 +122,37 @@ void file_control(const std::string& mode
     {
         file.save_all();
     }
+}
+
+std::vector<std::string> extract_args(std::istringstream& message)
+{
+    std::vector<std::string> to_return;
+    std::string holder{};
+
+    while(message >> holder)
+    {
+        to_return.push_back(holder);
+    }
+
+    return to_return;
+}
+
+std::string apply_function(const std::vector<std::string>& list
+                        , size_t expected_size
+                        , const Control_agregate& controls
+                        , std::string(*applier)
+                            (const Control_agregate& ,const std::vector<std::string>&))
+{
+    std::string feedback{};
+
+    if(expected_size == list.size())
+    {
+        feedback = applier(controls, list);
+    }   
+    else
+    {
+        feedback = "argument count not met\n";
+    }
+
+    return feedback;
 }
