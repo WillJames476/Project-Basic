1a9407c3c4bb21eb2ab7847107b57ba257d7c77d
WillJames476 <williamjames4@tutanota.com>
2022-02-09 01:38:57 +0800

    reduced the responsibilities of server s main function

--- src/Server/server_main.cpp
+++ src/Server/server_main.cpp
@@ -10,6 +10,10 @@
 #include "../includes/server_utilities.h"
 #include "server_control.h"
 
+void communication_handler(boost::asio::io_context& io_context
+                        , boost::system::error_code& err_codes
+                        , boost::asio::ip::tcp::acceptor& acceptor);
+
 int main(int argc, char** argv)
 {
     std::ios_base::sync_with_stdio(false);
@@ -19,28 +23,14 @@ int main(int argc, char** argv)
         try
         {
             std::string message{}, ip_addr{*(argv + 1)};
-
             boost::asio::io_context io_context{};
+
             boost::asio::ip::tcp::acceptor acceptor{io_context
                 , make_endpoint(ip_addr, 80)};
+
             boost::system::error_code err_codes;
-
-            while(true)
-            {
-                std::string received{};
-
-                boost::asio::ip::tcp::socket client{io_context};
-                acceptor.accept(client);
-
-                received = get_message(client, err_codes);
-                received = server_control(received);
-                send_message(client, err_codes, received);
-
-                client.close();
-                
-                break;
-            }
-
+            communication_handler(io_context, err_codes, acceptor);
+            
             acceptor.close();
         }
         catch(std::exception& excpt)
@@ -50,4 +40,26 @@ int main(int argc, char** argv)
     }
 
     return 0;
+}
+
+void communication_handler(boost::asio::io_context& io_context
+                        , boost::system::error_code& err_codes
+                        , boost::asio::ip::tcp::acceptor& acceptor)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::string received{};
+
+    while(true)
+    {
+        boost::asio::ip::tcp::socket client{io_context};
+        acceptor.accept(client);
+
+        received = get_message(client, err_codes);
+        received = server_control(received);
+        send_message(client, err_codes, received);
+
+        std::cout << received;
+        client.close();
+        break;
+    }
 }
