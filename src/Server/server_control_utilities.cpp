a750c6da6ac988a1675dbf3f02e3484a7fd29f63
willJames476 <williamjames4@tutanota.com>
2022-03-18 23:05:00 +0800

    continued refactoring of the server control utilities, some bug fixes

--- src/Server/server_control_utilities.cpp
+++ src/Server/server_control_utilities.cpp
@@ -12,66 +12,59 @@
 
 namespace ARGS_REGEX
 {
-    using namespace REGEX_PREDICATES;
+  using namespace REGEX_PREDICATES;
 
-    std::initializer_list<std::regex>
-        ACCOUNT        {ALPHA_NOSPACE, ALPHA_NOSPACE}
-        , COMMLINE     {ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE}
-	, COMMLINE_PUT {ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE, BOOLEAN}
-	, TASK         {ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE_S};
+  std::initializer_list<std::regex>
+  ACCOUNT        {ALPHA_NOSPACE, ALPHA_NOSPACE},
+    COMMLINE     {ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE},
+    COMMLINE_PUT {ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE, BOOLEAN},
+    TASK         {ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE, ALPHA_NOSPACE_S};
 }
 
 std::vector<std::string> extract_args(std::istringstream& message)
 {
-    std::vector<std::string> to_return;
-    std::string holder{};
+  std::vector<std::string> to_return;
+  std::string holder{};
 
-    while(message >> holder)
+  while(message >> holder)
     {
-        to_return.push_back(holder);
+      to_return.push_back(holder);
     }
 
-    return to_return;
+  return to_return;
 }
 
 std::string get_control(std::istringstream& message,
 			const View_agregate& view,
 			const Control_agregate& control)
 {
-    std::string application{};
-    message >> application;
-    auto args{extract_args(message)};
+  std::string application{};
+  message >> application;
+  auto args{extract_args(message)};
 
-    if(application == "--account")
-      {
-	application = apply_function(application, args,
-				     ARGS_REGEX::ACCOUNT,
-				     2, view,
-				     [&](const auto& x, const auto& y)
-				     {return get_account_info(x, control, y);});
-      }
-    else if(application == "--commline")
-      {
-	application = apply_function(application, args,
-				     ARGS_REGEX::ACCOUNT,
-				     2 , view,
-				     [&](const auto& x, const auto& y)
-				     {return get_commline_table(x, control,y);});
-      }
-    else if(application == "--todolist")
-      {
-	application = apply_function(application, args,
-				     ARGS_REGEX::ACCOUNT,
-				     2 , view,
-				     [&](const auto& x, const auto& y)
-										{
-											bool is_user_verified{control.account.get_from_list({y[0], y[1]})
-													!= "unsuccessfull operation"};
-
-											return is_user_verified ?
-													x.todolist.send_formatted(y[0])
-													: std::string{"failure"};
-										});
+  if(application == "--account")
+    {
+      application = apply_function(application, args,
+				   ARGS_REGEX::ACCOUNT,
+				   2, view,
+				   [&](const auto& x, const auto& y)
+				   {return get_account_info(x, control, y);});
+    }
+  else if(application == "--commline")
+    {
+      application = apply_function(application, args,
+				   ARGS_REGEX::ACCOUNT,
+				   2 , view,
+				   [&](const auto& x, const auto& y)
+				   {return get_commline_table(x, control,y);});
+    }
+  else if(application == "--todolist")
+    {
+      application = apply_function(application, args,
+				   ARGS_REGEX::ACCOUNT,
+				   2 , view,
+				   [&](const auto& x, const auto& y)
+				   {return get_todolist_list(x, control,y);});
     }
 
     return application;
@@ -80,74 +73,71 @@ std::string get_control(std::istringstream& message,
 std::string post_control(std::istringstream& message
                         , const Control_agregate& controls)
 {
-    std::string application{};
-    message >> application;
-    auto args{extract_args(message)};
+  std::string application{};
+  message >> application;
+  auto args{extract_args(message)};
 
-    if(application == "--account")
-      {
-        application = apply_function(application, args,
-				     ARGS_REGEX::ACCOUNT,
-				     2, controls ,
-				     [](const auto& x, const auto& y){return add_new_account(x, y);});
-      }
-    else if(application == "--commline")
-      {
-        application = apply_function(application, args,
-				     ARGS_REGEX::COMMLINE,
-				     3 ,controls,
-				     [](const auto& x, const auto& y){return add_new_commline(x, y);});
-      }
-    else if(application == "--todolist")
-      {
-	application = apply_function(application, args,
-				     ARGS_REGEX::TASK,
-				     4, controls,
-				     [](const auto& x, const auto& y){return add_new_task(x, y);});
-      }
+  if(application == "--account")
+    {
+      application = apply_function(application, args,
+				   ARGS_REGEX::ACCOUNT,
+				   2, controls ,
+				   [](const auto& x, const auto& y)
+				   {return add_new_account(x, y);});
+    }
+  else if(application == "--commline")
+    {
+      application = apply_function(application, args,
+				   ARGS_REGEX::COMMLINE,
+				   3 ,controls,
+				   [](const auto& x, const auto& y)
+				   {return add_new_commline(x, y);});
+    }
+  else if(application == "--todolist")
+    {
+      application = apply_function(application,args,
+				   ARGS_REGEX::TASK,
+				   4, controls,
+				   [](const auto& x, const auto& y)
+				   {return add_new_task(x, y);});
+    }
 
-    return application;
+  return application;
 }
 
 std::string delete_control(std::istringstream& message
                         , const Control_agregate& controls)
 {
-    std::string application{};
-    message >> application;
-    auto args{extract_args(message)};
+  std::string application{};
+  message >> application;
+  auto args{extract_args(message)};
 
-    if(application == "--account")
+  if(application == "--account")
     {
-        application = apply_function(application, args
-                        , ARGS_REGEX::ACCOUNT  
-                        , 2, controls 
-                        , [](const auto& x, const auto& y){return delete_account(x, y);});
+      application = apply_function(application, args,
+				   ARGS_REGEX::ACCOUNT,
+				   2, controls ,
+				   [](const auto& x, const auto& y)
+				   {return delete_account(x, y);});
     }
-    else if(application == "--commline")
+  else if(application == "--commline")
     {
-        application = apply_function(application, args
-                        , ARGS_REGEX::COMMLINE
-                        , 3 ,controls
-                        , [](const auto& x, const auto& y){return delete_a_line(x, y);});
+      application = apply_function(application, args,
+				   ARGS_REGEX::COMMLINE,
+				   3 ,controls,
+				   [](const auto& x, const auto& y)
+				   {return delete_a_line(x, y);});
     }
-	else if(application == "--todolist")
-	{
-		application = apply_function(application, args
-									, ARGS_REGEX::TASK
-									, 4, controls
-									, [](const auto& x, const auto& y)
-										{
-											bool is_loged_in{x.account.get_from_list({y[0], y[1]})
-													!= "unsuccessfull operation"}
-												, is_permitted{x.commline.is_user_permitted({y[0], y[2]})};
+  else if(application == "--todolist")
+    {
+      application = apply_function(application, args,
+				   ARGS_REGEX::TASK,
+				   4, controls
+				   , [](const auto& x, const auto& y)
+				   {return delete_a_task(x, y);});
+    }
 
-											return is_loged_in && is_permitted ?
-												x.todolist.remove_from_list_task({y[0], y[3], y[2]})
-												: std::string{};
-										});
-	}
-
-    return application;
+  return application;
 }
 
 std::string put_control(std::istringstream& message
@@ -180,12 +170,13 @@ std::string put_control(std::istringstream& message
 void file_control(const std::string& mode
                 , File_agregate& file)
 {
-    if(mode == "READ")
-      {
-	file.read_all();
-      }
-    else if(mode == "WRITE")
-      {
-	file.save_all();
-      }
+  if(mode == "READ")
+    {
+      file.read_all();
+    }
+  else if(mode == "WRITE")
+    {
+      file.save_all();
+    }
 }
+
